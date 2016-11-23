617484d31ccc38216833a4a642822d4d45630388
Alkagar <alkagar@gmail.com>
2016-11-23 12:03:52 +0100

    add configuration for watchOptions to npm run

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -235,7 +235,8 @@ function runDevServer(host, port, protocol) {
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
     watchOptions: {
-      ignored: /node_modules/
+      ignored: process.env.WEBPACK_WATCHOPTIONS_IGNORED || /node_modules/,
+      poll: process.env.WEBPACK_WATCHOPTIONS_POLL || undefined
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === "https",
